2c173985d9b85c578385ff0a351ca91a6c5869b6
arnobtarafder <rmtarnob@gmail.com>
2022-05-26 19:32:00 +0600

    update user method

--- index.js
+++ index.js
@@ -71,6 +71,21 @@ async function run() {
             res.send(users)
         });
 
+        app.put("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+
+            res.send({ result, token });
+        })
+
+
 
 
         console.log("connected to database");
