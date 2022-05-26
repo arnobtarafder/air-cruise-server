54cb0d45ca6b5e7b514fa849aef9dcca68a939ba
arnobtarafder <rmtarnob@gmail.com>
2022-05-26 17:09:46 +0600

    add: connection to database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
@@ -11,6 +12,29 @@ app.use(express.json())
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.otcue.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        await client.connect()
+        console.log("connected to database");
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
